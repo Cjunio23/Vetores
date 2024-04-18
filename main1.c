2532916125b3a184ddcdc42578373d46f44581d3
Custodio Junio <129317287+Cjunio23@users.noreply.github.com>
2024-04-18 18:23:51 -0300

    main1.c

--- main1.c
+++ main1.c
@@ -1,9 +1,9 @@
-/*Implemente a funçao,char *strstr (char *str1, char *str2)” que retorna o endereço de str1 em que ocorre pela primeira vez a
-substring str2. Caso a substring str2 não exista em str1, retorne NULL. Implemente o programa principal
-para testar a função. No programa principal, você deve exibir o endereço do primeiro caracter de str1 e
-também o endereço de str1 a partir do qual existe uma ocorrência de str2.*/
-//str1,mostrar o endereçeo do str1 na posicao 0
-//mostrar o endereço da primeira palavra na posiçao 0 e o segundo endereço que e o da palavra que voce esta procurando.
+/*Implemente a funÃ§ao,char *strstr (char *str1, char *str2)â€ que retorna o endereÃ§o de str1 em que ocorre pela primeira vez a
+substring str2. Caso a substring str2 nÃ£o exista em str1, retorne NULL. Implemente o programa principal
+para testar a funÃ§Ã£o. No programa principal, vocÃª deve exibir o endereÃ§o do primeiro caracter de str1 e
+tambÃ©m o endereÃ§o de str1 a partir do qual existe uma ocorrÃªncia de str2.*/
+//str1,mostrar o endereÃ§eo do str1 na posicao 0
+//mostrar o endereÃ§o da primeira palavra na posiÃ§ao 0 e o segundo endereÃ§o que e o da palavra que voce esta procurando.
 //exibir 2 endereco no final.Do inicio da frase e o da palavra que esta sendo procurada.
 //Se caso a segunda parte da procura nao exista,exibir null = vazio.
 //char = 1 byte
@@ -11,14 +11,14 @@ também o endereço de str1 a partir do qual existe uma ocorrência de str2.*/
 #include <stdlib.h>
 #include <string.h>
 char *ende(char *str1,char *str2)//Tive que trocar o nome,porque tem uma funcao com o nome strstr.
-//funcao strstr A função strstr devolve um ponteiro para a primeira ocorrência da string apontada por endereçoStrChave na string apontada por endereçoStrOrigem.
-// Ela devolve um ponteiro nulo de não for encontrada nenhuma coincidência.
+//funcao strstr A funÃ§Ã£o strstr devolve um ponteiro para a primeira ocorrÃªncia da string apontada por endereÃ§oStrChave na string apontada por endereÃ§oStrOrigem.
+// Ela devolve um ponteiro nulo de nÃ£o for encontrada nenhuma coincidÃªncia.
 {
     int tam, i=0, j=0, tamanho_str2 = strlen(str2);//tamanho da str2 igual a srtlen do str2,para ler o tamanho da string.
     char *ponteiro;
     for(i=0; i<strlen(str1); i++)//Para pecorer toda a string.
     {
-        if(str1[i]==str2[j])//os 2 na posiçao inicial.
+        if(str1[i]==str2[j])//os 2 na posiÃ§ao inicial.
         {
             if(j==0)//se j for igual a 0,ponteiro igual a str1 na posicao inicial.
             {
@@ -49,7 +49,7 @@ int main()
     caracter = ende(str1,str2);
      if(caracter!=NULL)//Caso for diferente de nulo,tenha encontrado a palavra digitada.
      {   printf("Endereco do primeiro caracter: %d\n",str1);//Mostra o endereco do primeiro caracter str1[i]
-         printf("endereco da primeira ocorrencia: %d\n",caracter);//endereço de str1 a partir do qual existe uma ocorrência de str2.
+         printf("endereco da primeira ocorrencia: %d\n",caracter);//endereÃ§o de str1 a partir do qual existe uma ocorrÃªncia de str2.
          printf("Caracter: %c\n",*caracter);//O primeiro caracter.
      }
      else
